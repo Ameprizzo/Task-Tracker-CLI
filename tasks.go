849ab3cece7dab79205e2651b2b7dfa4266ea8ef
Amedeus Lyakurwa <amedeus926@gmail.com>
2024-08-21 18:49:41 +0300

    Func to get all tasks from json file

--- tasks.go
+++ tasks.go
@@ -1,6 +1,10 @@
 package main
 
-import "time"
+import (
+	"encoding/json"
+	"os"
+	"time"
+)
 
 // Task struct represents a task with necessary fields
 type Task struct {
@@ -9,4 +13,22 @@ type Task struct {
 	Status      string    `json:"status"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+// Get all tasks from the JSON file
+func GetTasks() ([]Task, error) {
+	file, err := os.ReadFile("tasks.json")
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []Task{}, nil // No tasks.json file, return an empty list
+		}
+		return nil, err
+	}
+	var tasks []Task
+	err = json.Unmarshal(file, &tasks)
+	if err != nil {
+		return nil, err
+	}
+	return tasks, nil
+
 }
