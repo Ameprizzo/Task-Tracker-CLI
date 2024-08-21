a68fd8a59723c83cef0cfc1ea2ac7fe2c7b55684
Amedeus Lyakurwa <amedeus926@gmail.com>
2024-08-21 18:39:02 +0300

    Add task struct with neccessary fields

---
+++ tasks.go
@@ -0,0 +1,12 @@
+package main
+
+import "time"
+
+// Task struct represents a task with necessary fields
+type Task struct {
+	ID          int       `json:"id"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
